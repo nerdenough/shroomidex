1f243a05b9d1c401e1fa6ad9768b3cc5c60c9c1e
Brendan Goodenough <brendan@goodenough.nz>
2019-07-07 20:06:28 +1200

    Add header component with logo

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const Header = () => (
+  <header className="Header">
+    <img className="Header-logo" src="/logo.png" alt="Shroomidex" />
+  </header>
+)
+
+export default Header
