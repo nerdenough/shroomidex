3f543191ff8065e8ef77e6b29486245153281430
Brendan Goodenough <brendan@goodenough.nz>
2019-07-07 20:36:56 +1200

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const Footer = () => (
+  <footer className="Footer">
+    Made with&nbsp;
+    <img className="Footer-mushroom" src="./shroom-icon.png" alt="shrooms" />
+    &nbsp;by Brendan Goodenough
+  </footer>
+)
+
+export default Footer
